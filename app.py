3fe550e93bb1a9196870a71ba5b752f903459e4e
jaiswalpartha <parthajaiswal111@gmail>
2022-07-03 19:07:20 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 
 def index():
+    try:
+        raise Exception("testing custom exception")
+    except Exception  as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message) 
+        logging.info("testing our logging module")
     return "CI CD pipeline has been established"
 
 if __name__== "__main__":
